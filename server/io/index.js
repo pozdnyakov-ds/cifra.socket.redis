eaa2c8ebb434f434a6263bdeab0f458782379800
Dmitry <2903015@gmail.com>
2023-01-24 13:32:58 +0300

    v.2

--- server/io/index.js
+++ server/io/index.js
@@ -8,6 +8,9 @@ const io = require('socket.io')(server, {
 });
 const port = process.env.PORT || 4000;
 
+const redis = require('redis');
+const client = redis.createClient({url: 'redis://10.15.6.28:6380'});
+
 // app.get('/', (req, res) => {
 //   res.sendFile(__dirname + '/index.html');
 // });
@@ -15,6 +18,9 @@ const port = process.env.PORT || 4000;
 io.on('connection', (socket) => {
 
   socket.on('message', msg => {
+    d = Date();
+    client.set(d, JSON.stringify(msg));
+
     switch (msg.recipient) {
       case 'server':
         console.log(`We got message from ${msg.sender} to ${msg.recipient}: ${msg.message}`);
